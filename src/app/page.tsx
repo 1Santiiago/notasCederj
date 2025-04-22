75a98a8b081dd0b50e373b4cf374689be26d0277
Santiago Ferreira <santiago@orientme.com.br>
2025-04-22 17:33:54 -0300

    made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,111 @@
-import Image from "next/image";
+"use client";
+import { ModeToggle } from "@/components/modeToggle";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from "@radix-ui/react-dialog";
+import { DialogHeader } from "@/components/ui/dialog";
+import { useState } from "react";
+
+type FormInputs = {
+  ad1: number;
+  ap1: number;
+  ad2: number;
+  ap2?: number | undefined;
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [resultado, setResultado] = useState("");
+  const [open, setOpen] = useState(false);
+  const { register, handleSubmit, reset } = useForm<FormInputs>();
+  const onSubmit = (data: FormInputs) => {
+    const n1 = data.ad1 * 0.2 + data.ap1 * 0.8;
+    const ad2Peso = data.ad2 * 0.2;
+    const mediaMinima = 6;
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    const ap2Minima = (mediaMinima * 2 - n1 - ad2Peso) / 0.8;
+
+    const resultadoTexto =
+      ap2Minima > 10
+        ? "Infelizmente, não é possível alcançar a média 6 mesmo com nota máxima na AP2. <strong>Você precisa da AP3</strong>"
+        : `Você precisa tirar pelo menos <strong>${ap2Minima.toFixed(
+            2
+          )}</strong> na AP2 para ser aprovado.`;
+
+    setResultado(resultadoTexto);
+    setOpen(true);
+    reset();
+  };
+
+  return (
+    <div className=" lg:w-4/12 mx-auto py-10 w-full">
+      <Card>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <CardHeader>
+            <div className="flex justify-between align-middle ">
+              <CardTitle>Calcular nota</CardTitle>
+              <ModeToggle />
+            </div>
+            <CardDescription>
+              Descubra quanto precisa tirar na ap2 para ser aprovado
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-4">
+            <Label htmlFor="ad1">Nota da AD1</Label>
+            <Input
+              id="ad1"
+              placeholder="Digite sua nota AD1"
+              required
+              {...register("ad1")}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <Label htmlFor="ap1">Nota da AP1</Label>
+            <Input
+              id="ap1"
+              placeholder="Digite sua nota AP1"
+              required
+              {...register("ap1")}
+            />
+            <Label htmlFor="ad2">Nota da AD2</Label>
+            <Input
+              id="ad2"
+              placeholder="Digite sua nota AD2"
+              {...register("ad2")}
+            />
+          </CardContent>
+          <CardFooter className="py-4">
+            <Button type="submit">Calcular nota</Button>
+          </CardFooter>
+          <div className="flex justify-center">
+            <Dialog open={open} onOpenChange={setOpen}>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogDescription asChild>
+                    <div
+                      className="w-full p-10"
+                      dangerouslySetInnerHTML={{ __html: resultado }}
+                    />
+                  </DialogDescription>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog>
+          </div>
+        </form>
+      </Card>
     </div>
   );
 }
